d36ac47827aadcc7817f833937869e5dc49e84a3
ledung-29x7 <141516385+ledung-29x7@users.noreply.github.com>
2024-09-18 09:17:38 +0700

    first

---
+++ rapid-rescue/src/main/java/com/rapidrescue/rapid_rescue/RapidRescueApplication.java
@@ -0,0 +1,13 @@
+package com.rapidrescue.rapid_rescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidRescueApplication.class, args);
+	}
+
+}
